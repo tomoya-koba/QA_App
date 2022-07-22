18dde6c323a6a08e8f4ad202e46687d84afffcb8
tomoya-koba <kobatmya@hotmail.co.jp>
2022-07-22 10:04:36 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/tomoya/kobayashi5/qa_app/LoginActivity.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.tomoya.kobayashi5.qa_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
