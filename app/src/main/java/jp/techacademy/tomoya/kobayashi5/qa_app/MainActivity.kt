f7c69de8cfafe822a38aca7bf34c6f547fac94b3
tomoya-koba <kobatmya@hotmail.co.jp>
2022-07-22 19:45:52 +0900

    2nd

--- app/src/main/java/jp/techacademy/tomoya/kobayashi5/qa_app/MainActivity.kt
+++ app/src/main/java/jp/techacademy/tomoya/kobayashi5/qa_app/MainActivity.kt
@@ -1,23 +1,62 @@
 package jp.techacademy.tomoya.kobayashi5.qa_app
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar  // ← 追加
+import com.google.firebase.auth.FirebaseAuth
+// findViewById()を呼び出さずに該当Viewを取得するために必要となるインポート宣言
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {    // ← 修正
+
+    private var mGenre = 0    // ← 追加
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        // idがtoolbarがインポート宣言により取得されているので
+        // id名でActionBarのサポートを依頼
+        setSupportActionBar(toolbar)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        // fabにClickリスナーを登録
+        fab.setOnClickListener { view ->
+            // ジャンルを選択していない場合（mGenre == 0）はエラーを表示するだけ
+            if (mGenre == 0) {
+                Snackbar.make(view, getString(R.string.question_no_select_genre), Snackbar.LENGTH_LONG).show()
+            } else {
+
+            }
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
+
+            if (user == null) {
+                // ログインしていなければログイン画面に遷移させる
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                // ジャンルを渡して質問作成画面を起動する
+                val intent = Intent(applicationContext, QuestionSendActivity::class.java)
+                intent.putExtra("genre", mGenre)
+                startActivity(intent)
+            }
         }
+
+        // ～～ ここから
+        // ナビゲーションドロワーの設定
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.app_name, R.string.app_name)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        // ～～ ここまで
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -27,12 +66,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            val intent = Intent(applicationContext, SettingActivity::class.java)
+            startActivity(intent)
+            return true
         }
+
+        return super.onOptionsItemSelected(item)
     }
+
+    // ～～ ここから
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            toolbar.title = getString(R.string.menu_hobby_label)
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            toolbar.title = getString(R.string.menu_life_label)
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            toolbar.title = getString(R.string.menu_health_label)
+            mGenre = 3
+        } else if (id == R.id.nav_compter) {
+            toolbar.title = getString(R.string.menu_compter_label)
+            mGenre = 4
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    // ～～ ここまで
 }
